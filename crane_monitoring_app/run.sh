cacd953495835cb04196a8573e1cab5a00b7ddcb
alobanov <alobanov@bad.localdomain>
2022-06-01 23:39:31 +0300

    setup proxy

--- crane_monitoring_app/run.sh
+++ crane_monitoring_app/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "\nstarting up python server application api_server..." 
-gnome-terminal --tab --title="socket_data_server_test" --command="python3 ~/developer/python-proj/s7-data-server/api_server.py"
+gnome-terminal --tab --title="api_server" -- "cd '/home/alobanov/developer/python-proj/s7-data-server/' && ls && python3 api_server.py"
 echo "\nstarting up python server application socket_data_server..." 
-gnome-terminal --tab --title="socket_data_server_test" --command="python3 ~/developer/python-proj/s7-data-server/socket_data_server_test.py"
+gnome-terminal --tab --title="socket_data_server_test" -- "cd '/home/alobanov/developer/python-proj/s7-data-server/' && ls && python3 socket_data_server_test.py"
 
+exit 0
 cd ~/developer/flutter-proj/crane_monitoring_app/
 flutter upgrade
 flutter config --enable-linux-desktop
