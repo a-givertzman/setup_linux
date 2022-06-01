b379d15d9988dfb7c6b75663203a7f27ffe3414f
Anton Lobanov <a.givertzman@icloud.com>
2022-06-01 17:02:14 +0300

    setup proxy

--- crane_monitoring_app/setup_proxy.sh
+++ crane_monitoring_app/setup_proxy.sh
@@ -7,6 +7,8 @@ read useProxy
 
 if [[ $useProxy == 0  ]]; then
     echo "Continue installing without internet proxy..."
+    unset http_proxy https_proxy ftp_proxy rsync_proxy \
+          HTTP_PROXY HTTPS_PROXY FTP_PROXY RSYNC_PROXY
     git config --global --unset http.proxy
 elif [[ $useProxy == 1 ]]; then
     echo "trying to configure proxy..."
@@ -18,6 +20,20 @@ elif [[ $useProxy == 1 ]]; then
     read proxyUser
     echo -e -n "${GREEN}\tproxy user pass:${RESET}"
     read proxyPass
+
+    echo "\tconfiguring system proxy..."
+    local pre="$proxyUser:$proxyPass@"
+    local proxy=$pre$server:$port
+    export http_proxy=$proxy
+    export https_proxy=$proxy
+    export ftp_proxy=$proxy
+    export rsync_proxy=$proxy
+    export  HTTP_PROXY=$proxy
+    export HTTPS_PROXY=$proxy
+    export FTP_PROXY=$proxy
+    export RSYNC_PROXY=$proxy    
+
+    echo "\tconfiguring git proxy..."
     git config --global http.proxy $proxyUser:$proxyPass@$proxyIP:$proxyPort
 else
     echo "Continue installing without internet proxy..."
