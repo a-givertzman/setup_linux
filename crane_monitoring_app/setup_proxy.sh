87be8c88eb99bce4fa8f6c099e846924c6d2ae95
alobanov <alobanov@bad.localdomain>
2022-06-01 16:47:50 +0300

    setup proxy

--- crane_monitoring_app/setup_proxy.sh
+++ crane_monitoring_app/setup_proxy.sh
@@ -10,13 +10,13 @@ if [[ $useProxy == 0  ]]; then
     git config --global --unset http.proxy
 elif [[ $useProxy == 1 ]]; then
     echo "trying to configure proxy..."
-    echo -e "${GREEN}\tproxy server address:${RESET}"
+    echo -e -n "${GREEN}\tproxy server address:${RESET}"
     read proxyIP
-    echo -e "${GREEN}\tproxy port:${RESET}"
+    echo -e -n "${GREEN}\tproxy port:${RESET}"
     read proxyPort
-    echo -e "${GREEN}\tproxy user name:${RESET}"
+    echo -e -n "${GREEN}\tproxy user name:${RESET}"
     read proxyUser
-    echo -e "${GREEN}\tproxy user pass:${RESET}"
+    echo -e -n "${GREEN}\tproxy user pass:${RESET}"
     read proxyPass
     git config --global http.proxy $proxyUser:$proxyPass@$proxyIP:$proxyPort
 else
