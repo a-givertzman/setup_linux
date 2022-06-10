a6b12bbee011a79426a2ccca13536184035a2ed2
scada <scada@debian-ws.tkz>
2022-06-10 14:26:54 +0300

    run app

---
+++ crane_monitoring_app/run_api_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo -e "\nstarting up python server application api_server..." 
+python3 "/home/alobanov/developer/python-proj/s7-data-server/api_server.py"
+
---
+++ crane_monitoring_app/run_socket_data_server_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -e "\nstarting up python server application socket_data_server..." 
+python3 "/home/alobanov/developer/python-proj/s7-data-server/socket_data_server_test.py"
+
+
