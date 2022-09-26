a714c21efb4dba15f97cfe932029531474c88748
Anton Lobanov <a.givertzman@icloud.com>
2022-09-26 12:47:03 +0300

    autostart

--- crane_monitoring_app/start_scada_app.sh
+++ crane_monitoring_app/start_scada_app.sh
@@ -2,3 +2,5 @@
 
 echo -e "\nconfiguring touchscreen colibration matrix..."
 xinput set-prop 'DIALOGUE INC PenMount USB' 'Coordinate Transformation Matrix' 1.220 0.0 -0.10549 0.0 1.265934 -0.132143 0.0 0.0 1.0
+
+echo -e "\nstarting monitoring application..."
