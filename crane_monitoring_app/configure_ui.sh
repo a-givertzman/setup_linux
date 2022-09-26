6feadd0349632373fd42f80149d02f8b7be1a702
Anton Lobanov <a.givertzman@icloud.com>
2022-09-26 18:03:27 +0300

    Update configure_ui.sh

--- crane_monitoring_app/configure_ui.sh
+++ crane_monitoring_app/configure_ui.sh
@@ -3,4 +3,7 @@
 echo -e "\nconfiguring touchscreen colibration matrix..."
 xinput set-prop 'DIALOGUE INC PenMount USB' 'Coordinate Transformation Matrix' 1.220 0.0 -0.10549 0.0 1.265934 -0.132143 0.0 0.0 1.0
 
+echo -e "\nconfiguring display gamma values"
+xrandr --output eDP-1 --gamma 0.8:0.8:0.8
+
 exit 0
