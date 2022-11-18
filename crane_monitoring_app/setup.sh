5484512a84aa60d0e94f39bec37b984d06f11059
a-givertzman <a.givertzman@icloud.com>
2022-11-18 21:21:06 +0300

    install python 3.10.8 added

--- crane_monitoring_app/setup.sh
+++ crane_monitoring_app/setup.sh
@@ -60,11 +60,20 @@ echo -e "\nUpdating apt-get..."
 sudo apt update
 
 echo
-read -p "Install python ? 0 - no, 1 - yes: " installPython
+read -p "Install python3.10 ? 0 - no, 1 - yes: " installPython
 if [ $installPython == 0 ]; then
   echo -e "Cntinue without installing python"
 else
   echo -e "\n\ninstalling puthon not implemented yet!!!" 
+  sudo apt update  
+  sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+  wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz
+  tar -xvf Python-3.10.8.tgz
+  cd Python-3.10.8
+  sudo ./configure --enable-optimizations
+  sudo make -j 4 #nproc
+  sudo make altinstall
+  python3.10 --version
   echo -e "\n\not implemented yet!" 
 fi
 
